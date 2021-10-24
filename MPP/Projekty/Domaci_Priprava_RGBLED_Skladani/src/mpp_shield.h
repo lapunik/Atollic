de958305780c9ec0d5fe8920e7aa6a72b7494abb
Lapi <vojtechlapunik@gmail.com>
2021-10-24 04:50:28 +0200

    Atollic.

---
+++ MPP/Projekty/Domaci_Priprava_RGBLED_Skladani/src/mpp_shield.h
@@ -0,0 +1,27 @@
+/*
+ * mpp_shield.h
+ *
+ *  Created on: 28. 2. 2019
+ *      Author: Lapunik
+ */
+
+#ifndef MPP_SHIELD_H_
+#define MPP_SHIELD_H_
+
+#include "stm_core.h"
+
+#define RGBLED_RED  GPIOB,15
+#define RGBLED_GREEN  GPIOB,14
+#define RGBLED_BLUE  GPIOB,13
+
+#define BUTTON_RIGHT  GPIOB,4
+#define BUTTON_LEFT  GPIOB,5
+
+#define BUZZER  GPIOB,0
+
+void Inicializace8LED();
+void Write8LED(uint8_t value);
+
+
+
+#endif /* MPP_SHIELD_H_ */
